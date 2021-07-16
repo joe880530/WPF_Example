1017b2695f33b10e12c052de39ee0bd2cdffe5f6
joe880530 <joe880530@gmail.com>
2021-07-16 09:56:02 +0900

    - Event Routing Example.(Bubbling / Tunneling)  - Bubbling = 하위에서부터 순차적으로 이벤트 발생.  - Tunneling= 상위에서부터 순차적으로 이벤트 발생.

--- EventRouting_Example/MainWindow_Bubbling.xaml.cs
+++ EventRouting_Example/MainWindow_Bubbling.xaml.cs
@@ -18,9 +18,9 @@ namespace EventRouting_Example
     /// <summary>
     /// MainWindow.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow_Bubbling : Window
     {
-        public MainWindow()
+        public MainWindow_Bubbling()
         {
             InitializeComponent();
         }
